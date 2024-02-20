20c4507e1ca5a67b9941f100848aac5cd18b46d5
YushenHu0326 <62900433+YushenHu0326@users.noreply.github.com>
2024-02-20 02:03:50 -0500

    Update gravity_assist_simple.m
    Formalized the unit
    Fixed some error
    Played around a little with the params to get a better animation result

--- gravity_assist_simple.m
+++ gravity_assist_simple.m
@@ -2,50 +2,66 @@ function gravity_assist_simple()
 
 % constant
 
+% in Nm^2/kg^2
 G = 6.6743e-11;
 
-TIME_STEP_TOTAL = 1000;
+% in days
+TIME_STEP_TOTAL = 500;
+
+TIME_STEP = 60*60*24;
+
+% length scale in million km
+DISTANCE = 1e+9;
 
 % helper functions
 
 function gravity = calculateGravity(p1,p2,m,M)
-    d = [p2(1)-p1(1),p2(2)-p1(2),p2(3)-p1(3)];
+    % delta in position (million km)
+    d = p2-p1;
+    % convert million km to m
+    d = d*1e+9;
+    % calculate gravity in kgm/s^2
     g = G*m*M/(norm(d)*norm(d));
-    gravity = [g*d(1)/norm(d),g*d(2)/norm(d),g*d(2)/norm(d)];
+    gravity = g*d/norm(d);
 end
 
-function a = updateAcceleration(a0,m,f)
-    a = [a0(1)+f(1)/m,a0(2)+f(2)/m,a0(3)+f(3)/m];
-    a = [a(1)*1e-18,a(2)*1e-18,a(3)*1e-18];
+function a = updateAcceleration(m,f)
+    % calculate acceleration in m/s^2
+    a = f/m;
 end
 
 function v = updateVelocity(v0,a)
-    v = [v0(1)+a(1),v0(2)+a(2),v0(3)+a(3)];
+    % calculate velocity in million km/s
+    v = v0+a/DISTANCE;
 end
 
 function p = updatePosition(p0,v)
-    p = [p0(1)+v(1),p0(2)+v(2),p0(3)+v(3)];
+    % calculate position after a day
+    p = p0+v*TIME_STEP;
 end
 
 % properties
-% mass in kg
-% distance in million km (10^9 m)
 
+% mass in kg
 spacecraftM = 1000;
-spacecraftPos = [-0.5,-2,0];
-spacecraftVel = [0,0.02,0];
+% position in million km
+spacecraftPos = [-5,-.5,-1.8];
+% velocity in million km/s
+spacecraftVel = [0.15e-6,0.05e-6,0.05e-6];
+% acceleration in m/s^2
 spacecraftAcc = [0,0,0];
 
-planetM = 6.39e+23;
-planetPos = [0,0,0];
-planetVel = [0,0.005,0];
+% same for the planet
+planetM = 6.39e+28;
+planetPos = [0,-5,0];
+planetVel = [0,0.2e-6,0];
 planetAcc = [0,0,0];
 
 % animation
 
 curve1 = animatedline('LineWidth',1);
 curve2 = animatedline('LineWidth',1);
-set(gca,'XLim',[-20,20],'YLim',[-20,20],'ZLim',[-20,20]);
+set(gca,'XLim',[-10,10],'YLim',[-10,10],'ZLim',[-10,10]);
 
 hold on;
 grid on;
@@ -53,8 +69,8 @@ grid on;
 for i=1:TIME_STEP_TOTAL
     addpoints(curve1,spacecraftPos(1),spacecraftPos(2),spacecraftPos(3));
     addpoints(curve2,planetPos(1),planetPos(2),planetPos(3));
-    head1 = scatter3(spacecraftPos(1),spacecraftPos(2),spacecraftPos(3),1,'red');
-    head2 = scatter3(planetPos(1),planetPos(2),planetPos(3),10,'blue');
+    head1 = scatter3(spacecraftPos(1),spacecraftPos(2),spacecraftPos(3),10,'red');
+    head2 = scatter3(planetPos(1),planetPos(2),planetPos(3),100,'blue');
 
     drawnow
     pause(0.01);
@@ -70,11 +86,12 @@ for i=1:TIME_STEP_TOTAL
     gm = calculateGravity(spacecraftPos,planetPos,spacecraftM,planetM);
     gM = calculateGravity(planetPos,spacecraftPos,planetM,spacecraftM);
 
-    spacecraftAcc = updateAcceleration(spacecraftAcc,spacecraftM,gm);
-    planetAcc = updateAcceleration(planetAcc,planetM,gM);
+    spacecraftAcc = updateAcceleration(spacecraftM,gm);
+    planetAcc = updateAcceleration(planetM,gM);
 
     spacecraftVel = updateVelocity(spacecraftVel,spacecraftAcc);
     planetVel = updateVelocity(planetVel,planetAcc);
+    disp(norm(spacecraftVel));
 end
 
 end
